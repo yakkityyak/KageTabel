2ac3c574c0f1e899b1d0564ef6013be588809b38
Jakob <jakob.hjortshoj@noaignite.com>
2022-10-05 17:11:50 +0200

    Kage tabel der fetcher data fra jsson fil og understøtter desktop og mobil

---
+++ kageTabel/scripts.js
@@ -0,0 +1,56 @@
+"use strict";
+fetch("./data.json")
+.then((data) => {
+	
+	return data.json();
+})
+.then((objectData) => {
+	console.log(objectData[0]);
+	let tableData = "";
+	objectData.map((values) => {
+		tableData += ` <tr>
+		<td>${values.name}</td>
+		<td>${values.fat}</td>
+		<td>${values.protein}</td>
+		<td>${values.kcal}</td>
+		</tr>`;
+	});
+	document.getElementById("table_body").innerHTML = tableData;
+}).catch(err => {
+    console.log(err);
+  });
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    tBody.append(...sortedRows);
+
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
+
+
